7f1396f53ee724dd12258bb2ad09b3f1498c99d2
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-01 23:45:51 +0530

    Error type Implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,39 @@
 use hyper::{self, client::HttpConnector};
 
+use std::error::Error;
+use std::io;
+use std::io::Read;
+use std::fmt;
+
+use rustc_serialize::json;
+use rustc_serialize::json::Json;
+
+#[derive(Debug)]
+enum EsError{
+    EsError(String),
+    HttpError(hyper::Error),
+    IoError(io::Error),
+    JsonError(json::BuilderError)
+}
+
+impl From<io::Error> for EsError {
+    fn from(err: io::Error) -> EsError {
+        EsError::IoError(err)
+    }
+}
+
+impl From<hyper::Error> for EsError {
+    fn from(err: hyper::Error) -> EsError {
+        EsError::HttpError(err)
+    }
+}
+
+impl From<json::BuilderError> for EsError {
+    fn from(err: json::BuilderError) -> EsError {
+        EsError::JsonError(err)
+    }
+}
+
 struct Client {
     http_client: hyper::Client<HttpConnector>,
 }
